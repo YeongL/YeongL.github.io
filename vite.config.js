78043a727ed7a72fed9c37fbb4c89eee1eec7f53
YeongL <34955920+YeongL@users.noreply.github.com>
2026-06-27 14:38:20 +0900

    fix: base path for github.io

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/YeongL.github.io/',  // GitHub repo 이름에 맞게 수정
+  base: '/',  // GitHub repo 이름에 맞게 수정
 })
